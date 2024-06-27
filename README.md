3198a0b74ea94efcfb613e029e630ba9ec8b5b48
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2024-06-27 16:51:47 +0200

    add support for hex colors

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ SVG Recolor is a simple CLI tool to recolor SVG files. It is written in python.
 
 > [!NOTE]
 > Submodules have their own licenses and may not be covered by the license of this project. Check the submodule's license for details.
+
+## Example Usage
+
+```bash
+# Recolor the input SVG file to red and save it to the output file using RGB color values
+./colorize_svg.sh "example-input-svgs/input.svg" tmp/output.svg --color "255,0,0"
+
+# Recolor the input SVG file to red and save it to the output file using hex color values
+./colorize_svg.sh "example-input-svgs/input.svg" tmp/output.svg --color "#FF0000"
+```
--- colorize_svg.sh
+++ colorize_svg.sh
@@ -4,17 +4,41 @@ ARGPARSE_DESCRIPTION="Colorize SVG script"
 source $(dirname $0)/submodules/argparse-bash/argparse.bash || exit 1
 argparse "$@" <<EOF || exit 1
 parser.add_argument('input_svg', help='Input SVG file')
-parser.add_argument('red', help='Red component (0-255)', type=int)
-parser.add_argument('green', help='Green component (0-255)', type=int)
-parser.add_argument('blue', help='Blue component (0-255)', type=int)
 parser.add_argument('output_svg', help='Output SVG file')
+parser.add_argument('--color', required=True, help='Color in hex (e.g., #FF5733) or RGB (e.g., 255,125,0) format')
 EOF
 
 echo "Input SVG: $INPUT_SVG"
-echo "Red: $RED"
-echo "Green: $GREEN"
-echo "Blue: $BLUE"
+echo "Color: $COLOR"
 echo "Output SVG: $OUTPUT_SVG"
+
+# Function to check if color is in hex format
+is_hex_color() {
+    if [[ $1 =~ ^#?[0-9A-Fa-f]{6}$ ]]; then
+        return 0 # true
+    else
+        return 1 # false
+    fi
+}
+
+# Function to convert hex to RGB
+hex_to_rgb() {
+    local hexcolor="$1"
+    hexcolor="${hexcolor#\#}" # Remove '#' if present
+    local r=$((16#${hexcolor:0:2}))
+    local g=$((16#${hexcolor:2:2}))
+    local b=$((16#${hexcolor:4:2}))
+    echo "$r" "$g" "$b"
+}
+
+# Determine if color is hex or RGB and convert if necessary
+if is_hex_color "$COLOR"; then
+    read RED GREEN BLUE <<<$(hex_to_rgb "$COLOR")
+else
+    IFS=',' read -r RED GREEN BLUE <<<"$COLOR"
+fi
+
+echo "Red: $RED, Green: $GREEN, Blue: $BLUE"
 
 temp_grayscale_svg=$(mktemp)
 
