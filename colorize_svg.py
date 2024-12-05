8eb988815dd2d322370f0c154c7fb83b7a8f1b72
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2024-12-05 16:43:22 +0100

    add FastAPI endpoint "colorize"

---
+++ colorize_svg.py
@@ -0,0 +1,93 @@
+import argparse
+import subprocess
+import tempfile
+import os
+import sys
+
+
+# TODO: add support for 3 digit hex colors
+# TODO: Move to utils.py
+def is_hex_color(color):
+    return bool(
+        color.startswith("#")
+        and len(color) == 7
+        and all(c in "0123456789ABCDEFabcdef" for c in color[1:])
+    )
+
+
+# TODO: add support for 3 digit hex colors
+# TODO: Move to utils.py
+def hex_to_rgb(hexcolor):
+    hexcolor = hexcolor.lstrip("#")
+    return tuple(int(hexcolor[i : i + 2], 16) for i in (0, 2, 4))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Colorize SVG script")
+    parser.add_argument("input_svg", help="Input SVG file")
+    parser.add_argument("output_svg", help="Output SVG file")
+    parser.add_argument(
+        "--color",
+        required=True,
+        help="Color in hex (e.g., #FF5733) or RGB (e.g., 255,125,0) format",
+    )
+    args = parser.parse_args()
+
+    input_svg = args.input_svg
+    output_svg = args.output_svg
+    color = args.color
+
+    print(f"Input SVG: {input_svg}")
+    print(f"Color: {color}")
+    print(f"Output SVG: {output_svg}")
+
+    if is_hex_color(color):
+        red, green, blue = hex_to_rgb(color)
+    else:
+        red, green, blue = map(int, color.split(","))
+
+    print(f"Red: {red}, Green: {green}, Blue: {blue}")
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".svg") as temp_grayscale_svg:
+        temp_grayscale_svg_path = temp_grayscale_svg.name
+
+    try:
+        # Check if input file exists
+        if not os.path.isfile(input_svg):
+            print(f"Error: Input file '{input_svg}' does not exist.")
+            sys.exit(1)
+
+        # Convert to grayscale using svgray
+        print("Converting to grayscale...")
+        with open(temp_grayscale_svg_path, "w") as f:
+            subprocess.run(
+                ["python3", "./submodules/svgray/.svgray.py", input_svg],
+                check=True,
+                stdout=f,
+            )
+
+        # Add color using svgshift
+        print("Adding color...")
+        with open(output_svg, "w") as f:
+            subprocess.run(
+                [
+                    "./submodules/svgshift/svgshift.exe",
+                    "addrgb",
+                    str(red),
+                    str(green),
+                    str(blue),
+                    temp_grayscale_svg_path,
+                ],
+                stdout=f,
+                check=True,
+            )
+
+        print(f"Colorized SVG saved as {output_svg}")
+
+    finally:
+        print("Cleaning up...")
+        os.remove(temp_grayscale_svg_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,99 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+import subprocess
+import tempfile
+import os
+import re
+
+app = FastAPI()
+
+
+@app.post("/colorize")
+async def colorize(icon: UploadFile = File(...), color: str = "#FF0066"):
+    validate_icon(icon, color)
+
+    # Create temporary files for the input and output SVGs
+    with tempfile.NamedTemporaryFile(
+        delete=False, suffix=".svg"
+    ) as input_svg_temp, tempfile.NamedTemporaryFile(
+        delete=False, suffix=".svg"
+    ) as output_svg_temp:
+        try:
+            # Save the uploaded icon to the temporary input file
+            with open(input_svg_temp.name, "wb") as buffer:
+                buffer.write(icon.file.read())
+
+            # Run the colorize_svg.py script using bash
+            subprocess.run(
+                [
+                    "pipenv",
+                    "run",
+                    "colorize",
+                    input_svg_temp.name,
+                    output_svg_temp.name,
+                    "--color",
+                    color,
+                ],
+                check=True,
+            )
+
+            # Return the colorized SVG from the temporary output file
+            return FileResponse(
+                path=output_svg_temp.name,
+                media_type="image/svg+xml",
+                filename=icon.filename,
+            )
+        except subprocess.CalledProcessError as e:
+            raise HTTPException(
+                status_code=500,
+                detail="Error processing the SVG file.\nError: " + str(e),
+            )
+        finally:
+            # Clean up temporary files
+            try:
+                os.remove(input_svg_temp.name)
+                os.remove(output_svg_temp.name)
+            except PermissionError:
+                pass
+
+
+def validate_icon(icon, color):
+    # Validate file extension
+    if (
+        not icon.filename.lower().endswith(".svg")
+        or not icon.content_type == "image/svg+xml"
+    ):
+        raise HTTPException(
+            status_code=415,
+            detail="Only SVG files are supported. Make sure the file has a .svg file extension and is of type image/svg+xml.",
+        )
+    # Validate file size
+    if icon.size > 2048 * 2048:
+        raise HTTPException(
+            status_code=413,
+            detail="File size must be less than 2 MB. Make sure the SVG file is optimized before uploading.",
+        )
+    # Validate color format. For example, #FF0066 or 255,0,102
+    # TODO: add support for 3 digit hex colors: re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", color)
+    ## is six digit hex color
+    is_hex_color = re.search(r"#[a-fA-F0-9]{6}$", color)  # TODO: Move to utils.py
+    is_rgb_color = re.search(
+        r"^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9]),(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9]),(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$",
+        color,
+    )  # TODO: Move to utils.py
+    if not is_hex_color and not is_rgb_color:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid color format. Please provide a color in comma-separated RGB value (e.g., 255,0,102) or a hex color code (e.g., #FF0066, #F06).",
+        )
+
+
+def save_uploaded_file(upload_file: UploadFile, destination_path: str):
+    with open(destination_path, "wb") as buffer:
+        buffer.write(upload_file.file.read())
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
